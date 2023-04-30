c93105f184ced448ed991a465e4394576327495e
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-04-30 00:02:15 -0700

    Form validation

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/booking.css";
+import "./booking.css";
 const BookingForm = ({
   bookingData,
   setBookingData,
